901dfe3537da47718b015d40840653a7343b8238
Neill Barnard <>
2019-11-22 08:48:46 +0200

    asb werk

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "port":process.env.port,
+    "port":process.env.PORT,
     "mongoURL": "mongodb+srv://neill:neill@clust-drv6t.mongodb.net/Kyou?retryWrites=true&w=majority" //this must change per app
 }
